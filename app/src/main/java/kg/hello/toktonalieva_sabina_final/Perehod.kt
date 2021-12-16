c242d7ec2fb95b27b304b6aea3b176f3a8bb46e1
Sabubna <toktonalieva_sa@auca.kg>
2021-12-16 15:58:56 +0600

    second commit

--- app/src/main/java/kg/hello/toktonalieva_sabina_final/Perehod.kt
+++ app/src/main/java/kg/hello/toktonalieva_sabina_final/Perehod.kt
@@ -3,13 +3,14 @@ package kg.hello.toktonalieva_sabina_final
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 
 class Perehod : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perehod)
-        val textView = findViewById<ImageView>(R.id.imageView10)
+        val textView = findViewById<Button>(R.id.button)
 
         textView.setOnClickListener{
             startActivity(Intent(this,Perehod::class.java))
