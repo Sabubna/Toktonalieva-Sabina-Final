5e12ab1b1d842251194d05431c1f2ddeab4b50c9
Sabubna <toktonalieva_sa@auca.kg>
2021-12-16 16:12:03 +0600

    third commit

--- app/src/main/java/kg/hello/toktonalieva_sabina_final/MainActivity.kt
+++ app/src/main/java/kg/hello/toktonalieva_sabina_final/MainActivity.kt
@@ -1,11 +1,18 @@
 package kg.hello.toktonalieva_sabina_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageView = findViewById<ImageView>(R.id.imageView10)
+
+        imageView.setOnClickListener{
+            startActivity(Intent(this,Perehod::class.java))
+        }
     }
 }
--- app/src/main/java/kg/hello/toktonalieva_sabina_final/Perehod.kt
+++ app/src/main/java/kg/hello/toktonalieva_sabina_final/Perehod.kt
@@ -10,11 +10,8 @@ class Perehod : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perehod)
-        val textView = findViewById<Button>(R.id.button)
 
-        textView.setOnClickListener{
-            startActivity(Intent(this,Perehod::class.java))
-        }
+
 
     }
 
